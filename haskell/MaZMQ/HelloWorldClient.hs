9c599a7a24c120f0fa998941a0366a0638d9eb0d
Savor d'Isavano <newelevenken@163.com>
2015-09-29 14:42:05 +0800

    ZMQ HelloWorld C/S

---
+++ haskell/MaZMQ/HelloWorldClient.hs
@@ -0,0 +1,24 @@
+-- |
+-- Hello World client in Haskell
+-- Binds REQ socket to tcp://localhost:5555
+-- Sends "Hello" to server, expects "World" back
+-- //
+-- Originally translated to Haskell by ERDI Gergo http://gergo.erdi.hu///
+
+module Main where
+
+import System.ZMQ4.Monadic
+import Control.Monad (forM_)
+import Data.ByteString.Char8 (pack, unpack)
+
+main :: IO ()
+main =
+    runZMQ $ do
+        liftIO $ putStrLn "Connecting to Hello World server…"
+        reqSocket <- socket Req
+        connect reqSocket "tcp://localhost:5555"
+        forM_ [1..10] $ \i -> do
+            liftIO $ putStrLn $ unwords ["Sending request", show i]
+            send reqSocket [] (pack "Hello")
+            reply <- receive reqSocket
+            liftIO $ putStrLn $ unwords ["Received reply:", unpack reply]
---
+++ haskell/MaZMQ/HelloWorldServer.hs
@@ -0,0 +1,26 @@
+-- |
+-- Hello World server in Haskell
+-- Binds REP socket to tcp://*:5555
+-- Expects "Hello" from client, replies with "World"
+
+module Main where
+
+import System.ZMQ4.Monadic
+import Control.Monad (forever)
+import Data.ByteString.Char8 (pack, unpack)
+import Control.Concurrent (threadDelay)
+
+main :: IO ()
+main =
+    runZMQ $ do
+        liftIO $ putStrLn "Starting Hello World server"
+        repSocket <- socket Rep
+        bind repSocket "tcp://*:5555"
+        forever $ do
+            msg <- receive repSocket
+            (liftIO.putStrLn.unwords) ["Received request:", unpack msg]
+            liftIO $ putStrLn "Doing my job..."
+            -- Simulate doing some 'work' for 1 second
+            liftIO $ threadDelay (1 * 1000 * 1000)
+            liftIO $ putStrLn "Job done!"
+            send repSocket [] (pack "World")
