51c1b5ad1f7ad48440598839c28db2369f632b69
Savor d'Isavano <anohigisavay@gmail.com>
2016-01-28 00:50:39 +0800

    Start working on Rust

---
+++ rust/book/one/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let a = [1, 2, 3];
+    let b = [1, 2, 3, 4];
+    println!("Length of a is: {}", x(a));
+    println!("Length of b is: {}", x(b));
+    println!("Hello, world!");
+}
+
+fn x(a: [i32; 3]) -> usize {
+    let v = a.len() + 1;
+    fn l(a: [i32; v]) {
+        8
+    }
+}
